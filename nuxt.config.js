dbed355b275108900b760dc3570803ea6f9ff4e9
nori <nori901117@gmail.com>
2022-05-27 22:18:33 +0900

    fource commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ns-web-design',
+    title: "N's Design",
     htmlAttrs: {
       lang: 'ja',
     },
